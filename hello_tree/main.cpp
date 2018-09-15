a36e5c69968b511f1496cac63418b72d01ea71cb
陈宇 <chenyu@chenyudemacbook-pro.local>
2018-09-15 16:30:07 +0800

    Initial Commit

---
+++ hello_tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello_tree
+//
+//  Created by 陈宇 on 2018/9/15.
+//  Copyright © 2018年 陈宇. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
